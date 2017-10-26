885c19662d11cb0ecc3cb4e2f760e25cdf4b1a83
ch4newsteam <ryan-krutwig@uiowa.edu>
2017-10-26 11:18:24 -0500

    added table views and history of past session off profile

---
+++ teacherstudent/SessionHistroyViewController.swift
@@ -0,0 +1,26 @@
+//
+//  SessionHistroyViewController.swift
+//  teacherstudent
+//
+//  Created by  macbook_user on 10/26/17.
+//  Copyright © 2017  macbook_user. All rights reserved.
+//
+
+
+
+import Foundation
+import UIKit
+
+class SessionHistroyViewController: UIViewController {
+    //test
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    // testing
+}
