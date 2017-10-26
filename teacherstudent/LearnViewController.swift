5192fc03de36d1c6446d05321a3639dd8005d648
ch4newsteam <ryan-krutwig@uiowa.edu>
2017-10-26 11:27:24 -0500

    fished learn button

--- teacherstudent/LearnViewController.swift
+++ teacherstudent/LearnViewController.swift
@@ -16,9 +16,7 @@ class LearnViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
     
-    @IBAction func seaching(_ sender: UIButton) {
-        performSegue(withIdentifier: "teacherDataBaseSegue", sender: self)
-    }
+   
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
