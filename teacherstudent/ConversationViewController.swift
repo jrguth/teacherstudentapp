974c22bc64c18d93bebccd8958b0fea01e4ae74d
ch4newsteam <ryan-krutwig@uiowa.edu>
2017-10-30 19:42:48 -0500

    added the last view controller files

---
+++ teacherstudent/ConversationViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ConversationViewController.swift
+//  teacherstudent
+//
+//  Created by  macbook_user on 10/30/17.
+//  Copyright © 2017  macbook_user. All rights reserved.
+//
+import Foundation
+import UIKit
+
+class ConversationViewController: UIViewController {
+    //test
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    // testing
+}
